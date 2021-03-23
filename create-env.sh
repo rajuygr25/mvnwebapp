8ec2d1aa363f109e6c2f2fb9595db8a660136a3d
rajuygr25 <76805713+rajuygr25@users.noreply.github.com>
2021-03-23 23:00:20 +0530

    Update create-env.sh
    Updated

--- create-env.sh
+++ create-env.sh
@@ -5,10 +5,10 @@ sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating www.Kart$i container.."
+    echo "Creating www.kart$i container.."
     sleep 1
-    docker run --name www.Kart$i -d -it rajuygr/Kart-img /bin/bash
-    echo "www.Kart$i container has been created!"
+    docker run --name www.kart$i -d -it rajuygr/kart-img /bin/bash
+    echo "www.kart$i container has been created!"
 	echo "=============================="
 done
 docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -q` > IPs.txt
